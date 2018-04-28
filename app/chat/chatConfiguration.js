fa948f1de12e7ad1c9ba7031cf2ce40e19b05b7e
williamhaynes <w.haynes@rgu.ac.uk>
2018-04-28 11:37:00 +0100

    Removal of some keys

--- app/chat/chatConfiguration.js
+++ app/chat/chatConfiguration.js
@@ -4,8 +4,8 @@ module.exports = {
     rltm: {
         service: "pubnub",
         config: {
-            publishKey: "pub-c-105753b1-e7e4-48c5-a55e-99c728d7e28a",
-            subscribeKey: "sub-c-fa62a58e-1ede-11e8-9bf4-060db84035e6"
+            publishKey:
+            subscribeKey:
         }
     }
 };*/
